9b8776779b8eb4ccbadc172dea9bce57a4f5c27d
Kirill Baranov <k.baranov@aladdin.ru>
2023-10-12 18:35:20 +0300

    Итоговый проект - наброски

--- project/init/exporter.sql
+++ project/init/exporter.sql
@@ -1,7 +1,5 @@
-CREATE USER 'exporter'@'mysql-exporter' IDENTIFIED BY 'exporter' WITH MAX_USER_CONNECTIONS 3;
-GRANT PROCESS, REPLICATION CLIENT, SELECT ON *.* TO 'user_wp'@'%';
-GRANT PROCESS, REPLICATION CLIENT ON *.* TO 'exporter'@'mysql-exporter';
-GRANT SELECT ON performance_schema.* TO 'user_wp'@'%';
-GRANT SELECT ON performance_schema.* TO 'exporter'@'mysql-exporter';
+CREATE USER 'exporter'@'%' IDENTIFIED BY 'exporter' WITH MAX_USER_CONNECTIONS 3;
+GRANT PROCESS, REPLICATION CLIENT ON *.* TO 'exporter'@'%';
+GRANT SELECT ON performance_schema.* TO 'exporter'@'%';
 FLUSH PRIVILEGES;
 EXIT;
